8fe9c53c5e9c13b3b038ccdf4aac2fc74800e146
hasanali117 <hassanali61911@gmail.com>
2023-07-05 01:28:33 +0300

    add navbar components + add content to home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,24 +1,11 @@
 import React, { Component } from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import Slide from "./Slide";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <Navbar expand="lg" bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="#link">Anime</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <Slide />
       </div>
     );
   }
---
+++ frontend/src/components/Signin.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Signin extends Component {
+  render() {
+    return <div>Signin</div>;
+  }
+}
