48852dc22e6df2bb48786ac9c2ce1e54fc2b02c3
Ahmed Home PC <a7med3365@gmail.com>
2023-07-05 03:36:16 +0300

    added a test on hover video player component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Slide from "./Slide";
-
+import Video from "./Video";
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <Slide />
+        <Video />
+        {/* <Slide /> */}
       </div>
     );
   }
