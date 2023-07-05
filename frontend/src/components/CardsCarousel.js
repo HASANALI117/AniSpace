0bb8b6394b0cb01b332f1904d0b623abf95abebd
Ahmed Home PC <a7med3365@gmail.com>
2023-07-06 00:10:54 +0300

    made the cards carousel useabl for any anime list

--- frontend/src/components/CardsCarousel.js
+++ frontend/src/components/CardsCarousel.js
@@ -1,16 +1,9 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import AnimeCard from "./AnimeCard";
-import { RxDoubleArrowRight } from "react-icons/rx";
-import { RxDoubleArrowLeft } from "react-icons/rx";
 
 export default function CurrentSeasonCarousel(props) {
-  const [animeList, setAnimeList] = useState([]);
-  const [error, setError] = useState(null);
-
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
@@ -31,59 +24,40 @@ export default function CurrentSeasonCarousel(props) {
     },
   };
 
-  useEffect(() => {
-    axios
-      .get("https://api.jikan.moe/v4/seasons/now")
-      .then((response) => {
-        console.log(response.data.data);
-        setAnimeList(response.data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        setError(error);
-      });
-  }, []);
+  const animeCards = props.animeList.map((anime) => (
+    <div
+      style={{
+        margin: "0 20px",
+        padding: "2rem 0",
+        height: "37vh",
+        width: "11vw",
+      }}
+    >
+      <AnimeCard key={anime.mal_id} anime={anime} />
+    </div>
+  ));
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (animeList.length === 0) {
-    return <div>Loading...</div>;
-  } else {
-    const animeCards = animeList.map((anime) => (
-      <div
-        style={{
-          margin: "0 20px",
-          padding: "2rem 0",
-          height: "37vh",
-          width: "11vw",
-        }}
+  return (
+    <div>
+      <div className="carousel-title">{props.carouselTitle}</div>
+      <Carousel
+        swipeable={true}
+        draggable={false}
+        showDots={false}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        deviceType={props.deviceType}
+        keyBoardControl={true}
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
       >
-        <AnimeCard key={anime.mal_id} anime={anime} />
-      </div>
-    ));
-
-    return (
-      <div>
-        <div className="carousel-title">Current Season</div>
-        <Carousel
-          swipeable={true}
-          draggable={false}
-          showDots={false}
-          responsive={responsive}
-          ssr={true} // means to render carousel on server-side.
-          infinite={true}
-          deviceType={props.deviceType}
-          keyBoardControl={true}
-          transitionDuration={500}
-          containerClass="carousel-container"
-          removeArrowOnDeviceType={["tablet", "mobile"]}
-          dotListClass="custom-dot-list-style"
-          itemClass="carousel-item-padding-40-px"
-        >
-          {animeCards}
-        </Carousel>
-        ;
-      </div>
-    );
-  }
+        {animeCards}
+      </Carousel>
+      ;
+    </div>
+  );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,53 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Slide from "./Slide";
 import Video from "./Video";
 import CardsCarousel from "./CardsCarousel";
+import axios from "axios";
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        {/* <Video /> */}
-        <Slide />
-        <CardsCarousel />
-      </div>
-    );
+export default function Home() {
+  const [airingAnimeList, setAiringAnimeList] = useState([]);
+  const [topAnimeList, setTopAnimeList] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://api.jikan.moe/v4/seasons/now")
+      .then((response) => {
+        console.log(response.data.data);
+        setAiringAnimeList(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+      });
+
+    axios
+      .get("https://api.jikan.moe/v4/top/anime")
+      .then((response) => {
+        console.log(response.data.data);
+        setTopAnimeList(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+      });
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (airingAnimeList.length === 0) {
+    return <div>Loading...</div>;
   }
+
+  return (
+    <div>
+      {/* <Video /> */}
+      <Slide />
+      <CardsCarousel
+        animeList={airingAnimeList}
+        carouselTitle={"Current Season"}
+      />
+      <CardsCarousel animeList={topAnimeList} carouselTitle={"Top Anime"} />
+    </div>
+  );
 }
